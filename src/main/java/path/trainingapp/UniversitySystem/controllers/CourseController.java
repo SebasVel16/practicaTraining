2cd405e2874bb3f6e04ab728b7a8c51d75b84aac
sebastian.velezp <sebastian.velezp@perficient.com>
2022-08-10 15:08:01 -0500

    Implemented method on controller to get course by passing the id on the url

--- src/main/java/path/trainingapp/UniversitySystem/controllers/CourseController.java
+++ src/main/java/path/trainingapp/UniversitySystem/controllers/CourseController.java
@@ -3,19 +3,25 @@ package path.trainingapp.UniversitySystem.controllers;
 import org.springframework.web.bind.annotation.*;
 import path.trainingapp.UniversitySystem.dto.CourseDTO;
 import path.trainingapp.UniversitySystem.dto.CourseSubjectDTO;
+import path.trainingapp.UniversitySystem.exceptions.ResourceNotFoundException;
+import path.trainingapp.UniversitySystem.mapper.CourseMapper;
+import path.trainingapp.UniversitySystem.models.Course;
 import path.trainingapp.UniversitySystem.services.CourseService;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
 
     private final CourseService courseService;
+    private final CourseMapper courseMapper;
 
-    public CourseController(CourseService courseService) {
+    public CourseController(CourseService courseService, CourseMapper courseMapper) {
         this.courseService = courseService;
+        this.courseMapper = courseMapper;
     }
 
 
@@ -33,6 +39,16 @@ public class CourseController {
     @PostMapping("/register-subjects")
     public String registerSubject(@Valid @RequestBody CourseSubjectDTO courseSubjectDTO){
             return courseService.registerSubject(courseSubjectDTO);
+    }
+
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable Long id){
+       Optional<Course> course = courseService.getCourse(id);
+       if(course.isPresent()){
+           return courseMapper.courseToCourseDTO(course.get());
+       }else {
+           throw new ResourceNotFoundException("Course not found");
+       }
     }
 
 }
